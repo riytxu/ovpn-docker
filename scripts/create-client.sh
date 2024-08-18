e779a4fac90fb8b1aa9304124f2a56a094dc570e
Aleksey Balahnin <alesha961337@yandex.ru>
2024-08-18 09:15:41 +0300

    Init

---
+++ scripts/create-client.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker compose run --rm openvpn easyrsa build-client-full $1 nopass
+docker compose run --rm openvpn ovpn_getclient $1 > $1.ovpn
---
+++ scripts/remove-client.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker compose run --rm openvpn ovpn_revokeclient $1 remove
